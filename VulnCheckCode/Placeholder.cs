2a71093f00dea31c8a13d3eaceacc28a0e3724cc
Tyler Brown <tybrown@mines.edu>
2026-03-25 20:47:26 -0400

    Initial commit with basic updates for local dev

---
+++ VulnCheckCode/Placeholder.cs
@@ -0,0 +1,6 @@
+﻿namespace VulnCheck.VulnCheckCode;
+
+public class Placeholder
+{
+    //You're recommended but not required to keep all your code in this package and all your assets in the VulnCheck folder.
+}
