f9a8c8d225e6a048bcb3a56b98a60524900952cf
Tyler Brown <tybrown@mines.edu>
2026-04-01 23:32:35 -0400

    Adding notes

--- VulnCheckCode/BuffTracker.cs
+++ VulnCheckCode/BuffTracker.cs
@@ -27,6 +27,12 @@ class BuffTracker
             var buffCards = __result.Result;
             foreach (var card in buffCards)
             {
+                // TODO FUCK THIS WON'T WORK FOR MULTIPLAYER AT ALL I DON'T THINK.  Draw is probably client sided?
+                // TODO I don't think this will get called in multiplayer in a way that works for us
+                // TODO rework so that instead of manually checking Vulnerable we get a list of matchers to check the card against
+                // TODO we can add a flag if the value exists on the card and then add UI elements based on the flags
+                // TODO this works reasonably well for singleplayer but will need to figure out a way to make it work with multiplayer
+                // TODO Then we can just have the mod config have the enable/disable for each buff we want to look for
                 try
                 {
                     if (card.DynamicVars.Vulnerable.BaseValue > 0)
